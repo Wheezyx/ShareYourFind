94a32b514826b4c53f7665faa92b776712bfb1b0
Wheezyx <mateusz.wedel@gmail.com>
2017-11-20 18:54:17 +0100

    Configuring DataSource and ConnectionProvider class

--- src/util/ConnectionProvider.java
+++ src/util/ConnectionProvider.java
@@ -1,5 +1,34 @@
 package util;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ConnectionProvider {
+
+    private static DataSource dataSource;
+
+    public Connection getConnection() throws SQLException, NamingException {
+        return getDataSource().getConnection();
+    }
+
+    public static DataSource getDataSource() throws NamingException {
+        if (dataSource == null)
+        {
+            try {
+            Context context = new InitialContext();
+            Context con = (Context) context.lookup("java:comp/env");
+            DataSource ds = (DataSource) con.lookup("jdbc/shareyourfind");
+            dataSource = ds;
+            } catch (NamingException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return dataSource;
+    }
 
 }
